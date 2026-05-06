c1942c10fc07202e7628752e61c5c150d4e691e1
Khanal-964 <khanalb964@gmail.com>
2026-05-06 22:59:25 +0530

    Time: 47 ms (5.15%), Space: 42.4 MB (5.65%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+
+        if(num==1){
+            return true;
+        }
+        int start=1;
+        int end=num/2;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(num/mid==mid&&num%mid==0){
+                return true;
+            }
+            else if(num/mid<mid){
+                end =mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return false;
+    }
+}
